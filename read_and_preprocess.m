67d81297dc13120d6a74032019e15b8a31033951
remco <remco.niggebrugge@gmail.com>
2015-04-11 06:43:10 +0200

    modularize

--- read_and_preprocess.m
+++ read_and_preprocess.m
@@ -1,10 +1,7 @@
-function [X Y Xmax ] = read_and_preprocess(filename)
+function [X Y ] = read_and_preprocess(filename)
 	X = csvread(filename);
 	X(1,:) = []; 	%% remove first row (labels)
 	X(:,1) = []; 	%% remove first column (ids)
 
 	[X, Y] = add_features(X);
-	Xmax = max(X);
-	X = X./Xmax;
-
 end
--- take_random_parts.m
+++ take_random_parts.m
@@ -2,8 +2,13 @@ function [Xtrain Xtest] = take_random_parts(filename, mtrain, mtest)
 	X = csvread(filename);
 	X(1,:) = []; %% remove first line (labels)
 	X(:,1) = []; %% remove first column (ids)
-	v = randperm(size(X,1));
-	X = X(v,:);
-	Xtrain = X(1:mtrain,:);
-	Xtest = X((mtrain+1):(mtrain+mtest),:);
+	if mtrain == 0
+		Xtrain = X;
+		Xtest = [];
+	else
+		v = randperm(size(X,1));
+		X = X(v,:);
+		Xtrain = X(1:mtrain,:);
+		Xtest = X((mtrain+1):(mtrain+mtest),:);
+	end
 end
