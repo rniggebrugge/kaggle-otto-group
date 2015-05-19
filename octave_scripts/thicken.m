099e63455bf97625a4460d42ee3b19fb26008248
remco <remco.niggebrugge@gmail.com>
2015-05-19 21:56:18 +0200

    thicken image

---
+++ octave_scripts/thicken.m
@@ -0,0 +1,17 @@
+function xthick = thicken(x)
+
+	[m,n] = size(x);
+	xthick = x;
+	xthick(xthick~=0)=1;
+	xthick = int8(xthick);
+
+	xleft = [xthick(:, 2:n) zeros(m,1)];
+	xright = [zeros(m,1) xthick(:, 1:n-1) ];
+	xup = [xthick(:, 29:n) zeros(m,28)];
+	xdown = [zeros(m,28) xthick(:,1:n-28)];
+
+	xthick = xthick + xleft + xright + xup + xdown;
+
+	xthick(xthick~=0)=1;
+
+end
