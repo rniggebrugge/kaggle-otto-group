c47133bac54c225743d69f42a916ea25c61dbc65
remco <remco.niggebrugge@gmail.com>
2015-05-21 21:59:35 +0200

    classify...

--- octave_scripts/ada_nn_classify.m
+++ octave_scripts/ada_nn_classify.m
@@ -1,4 +1,4 @@
-function [pred, h2, progress] = ada_nn_classify(...
+function [pred, h2, progress, count] = ada_nn_classify(...
 	x,y, ...
 	model_theta_1, ...
 	model_theta_2, ...
@@ -13,23 +13,21 @@ function [pred, h2, progress] = ada_nn_classify(...
 	h2   = zeros(m,10);
 	pred = [];
 	progress = [];
-
+	count = zeros(m,10);
 
 	for ix = 1:n
-
 		theta1 = model_theta_1{ix};
 		theta2 = model_theta_2{ix};
 		feats  = fs(ix,:);
 		[p h] = predict(theta1, theta2, x(:,feats));
 		pred = [pred p];
 		% h     = h./sum(h,2);
-		h2    = h2 + alpha(ix)*h;
+		h2    = h2 + h; % alpha(ix)*h;
 		[dd p_] = max(h2, [], 2);
+		count += eye(10)(p,:);
 		progress=[progress mean(p_==y)];
-
 	end
 
 	h2 = h2./sum(h2,2);
 	% [dummy, pred] = max(h2, [], 2);
-
 end
