6af56474c7e70c5e1d9c2f9c26a2217cb2168126
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-05-29 14:13:31 +0200

    ensemble

--- octave_scripts/digit_shape.m
+++ octave_scripts/digit_shape.m
@@ -7,23 +7,32 @@ function xout = digit_shape(xin)
 	activated_v = zeros(m,1);
 	all_h = [];
 	all_v = [];
+	sum_h = [];
+	sum_v = [];
 
 	for y=1:28
 		temp_h = zeros(m,1);
 		temp_v = zeros(m,1);
+		temp_sum_h = zeros(m,1);
+		temp_sum_v = zeros(m,1);
 		for x=1:28
 			colnumber_h = x+(y-1)*28;
 			column_h = xin(:,colnumber_h);
+			temp_sum_h += column_h;
 			temp_h = temp_h + xor(activated_h, column_h);
 			activated_h = column_h;
 			colnumber_v = y+(x-1)*28;
 			column_v = xin(:,colnumber_v);
+			temp_sum_v += column_v;
 			temp_v = temp_v + xor(activated_v, column_v);
 			activated_v = column_v;
 		end
 		all_h=[all_h temp_h/2 ];
 		all_v=[all_v temp_v/2 ];
+		sum_h=[sum_h temp_sum_h];
+		sum_v=[sum_v temp_sum_v];
 	end
+
 
 	% for ix=1:m
 	% 	v = all_h(ix,:);
@@ -36,7 +45,7 @@ function xout = digit_shape(xin)
 	% 	all_v(ix,:) = [v zeros(1,first-1)];
 	% end
 
-	xout=[all_h all_v];
+	xout=[all_h all_v sum_h sum_v];
 end
 
 
