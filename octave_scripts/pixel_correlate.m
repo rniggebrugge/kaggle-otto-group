d5da73c77ca6a8c7383f05daceee27dd0ee1813b
remco <remco.niggebrugge@gmail.com>
2015-05-30 10:51:32 +0200

    ..

---
+++ octave_scripts/pixel_correlate.m
@@ -0,0 +1,18 @@
+function xout = pixel_correlate(x)
+
+	step = 5;
+	x(x~=0)=1;
+	x(x==0)=-1;
+	xout = [];
+
+	for iy=0:27-step
+		for ix=0:27-step
+			my_col = ix+28*iy+1;
+			col_dx = my_col+step;
+			col_dy = my_col+step*28;
+			xout = [xout x(:,my_col).*x(:,col_dx) x(:,my_col).*x(:,col_dy)];
+		end
+	end
+end
+
+
